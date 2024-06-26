40e34c0f50ce15a26127d0f38c058dcc5f22fa53
Raphaël Tatin <raphael.tatin@seek-one.fr>
2024-06-26 16:42:47 +0200

    Add translations of all source files

--- ts/fr.ts
+++ ts/fr.ts
@@ -1,56 +1,105 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="fr_FR">
 <context>
     <name>Main</name>
     <message>
-        <location filename="../src/main.cpp" line="142"/>
         <source>Error</source>
         <translation>Erreur</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="142"/>
         <source>Error to initialize application</source>
-        <translation>Erreur pour initialiser l'application</translation>
+        <translation>Erreur lors de l&apos;initialisation de l&apos;application</translation>
     </message>
 </context>
 <context>
     <name>QAboutDialog</name>
     <message>
-        <location filename="../src/GUI/QAboutDialog.cpp" line="45"/>
         <source>This program is distributed under the terms of the &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.fr.html&quot;&gt;GNU GPL version 3&lt;/a&gt;.</source>
         <translation>Ce programme est distribué sous les termes de la &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.fr.html&quot;&gt;GNU GPL version 3&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QAboutDialog.cpp" line="46"/>
         <source>%1 is a product created and sponsored by %3: &lt;a href=&quot;http://%4&quot;&gt;%4&lt;/a&gt;</source>
         <translation>%1 est un produit créé et sponsorisé par %3: &lt;a href=&quot;http://%4&quot;&gt;%4&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QAboutDialog.cpp" line="48"/>
         <source>This product uses open source softwares:</source>
-        <translation>Ce produit utilise des logiciels libres :</translation>
+        <translation>Ce produit utilise des logiciels open source:</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QAboutDialog.cpp" line="79"/>
         <source>About %1</source>
-        <translation >A propos de %1</translation>
+        <translation>A propos %1</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QAboutDialog.cpp" line="107"/>
         <source>About Qt</source>
         <translation>A propos de Qt</translation>
     </message>
 </context>
 <context>
+    <name>QBytesView</name>
+    <message>
+        <source>Cannot display data representation for more than 64 bits</source>
+        <translation>Impossible d&apos;afficher la représentation des données sur plus de 64 bits</translation>
+    </message>
+    <message>
+        <source>BigEndian</source>
+        <translation>BigEndian</translation>
+    </message>
+    <message>
+        <source>LittleEndian</source>
+        <translation>LittleEndian</translation>
+    </message>
+    <message>
+        <source>Signed Integer</source>
+        <translation>Entier signé</translation>
+    </message>
+    <message>
+        <source>Unsigned Integer</source>
+        <translation>Entier non signé</translation>
+    </message>
+    <message>
+        <source>Float</source>
+        <translation>Flottant</translation>
+    </message>
+    <message>
+        <source>Hexadecimal</source>
+        <translation>Hexadécimal</translation>
+    </message>
+    <message>
+        <source>Octal</source>
+        <translation>Octal</translation>
+    </message>
+    <message>
+        <source>Binary</source>
+        <translation>Binaire</translation>
+    </message>
+</context>
+<context>
+    <name>QBytesViewController</name>
+    <message>
+        <source>Signed Integer (%1 bits)</source>
+        <translation>Entier signé (%1 bits)</translation>
+    </message>
+    <message>
+        <source>Unsigned Integer (%1 bits)</source>
+        <translation>Entier non signé (%1 bits)</translation>
+    </message>
+    <message>
+        <source>Float (%1 bits)</source>
+        <translation>Flottant (%1 bits)</translation>
+    </message>
+</context>
+<context>
     <name>QFileStructureView</name>
     <message>
-        <location filename="../src/GUI/QFileStructureView.cpp" line="36"/>
         <source>Refresh</source>
         <translation>Rafraîchir</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QFileStructureView.cpp" line="50"/>
+        <source>Open</source>
+        <translation>Ouvrir</translation>
+    </message>
+    <message>
         <source>Load</source>
         <translation>Charger</translation>
     </message>
@@ -58,67 +107,252 @@
 <context>
     <name>QFileStructureViewController</name>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="146"/>
+        <source>XML files (*.xml)</source>
+        <translation>Fichiers XML (*.xml)</translation>
+    </message>
+    <message>
+        <source> (Open by user)</source>
+        <translation> (Ouvert par l&apos;utilisateur)</translation>
+    </message>
+    <message>
         <source>Name</source>
         <translation>Nom</translation>
     </message>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="147"/>
         <source>Type</source>
         <translation>Type</translation>
     </message>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="148"/>
         <source>Size</source>
         <translation>Taille</translation>
     </message>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="149"/>
         <source>Offset</source>
         <translation>Position</translation>
     </message>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="150"/>
         <source>Value</source>
         <translation>Valeur</translation>
     </message>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="216"/>
         <source>Error</source>
         <translation>Erreur</translation>
     </message>
     <message>
-        <location filename="../src/GUIController/QFileStructureViewController.cpp" line="216"/>
         <source>Error while creating the structure of the file</source>
         <translation>Erreur à la création de la structure du fichier</translation>
     </message>
 </context>
 <context>
+    <name>QFileViewController</name>
+    <message>
+        <source>No file loaded</source>
+        <translation>Aucun fichier chargé</translation>
+    </message>
+    <message>
+        <source>No match found</source>
+        <translation>Aucun résultat trouvé</translation>
+    </message>
+</context>
+<context>
+    <name>QFindDialog</name>
+    <message>
+        <source>Find Previous</source>
+        <translation>Rechercher le précédent</translation>
+    </message>
+    <message>
+        <source>Find Next</source>
+        <translation>Rechercher le suivant</translation>
+    </message>
+</context>
+<context>
+    <name>QGoToBytes</name>
+    <message>
+        <source>Offset type :</source>
+        <translation>Type de position :</translation>
+    </message>
+    <message>
+        <source>Hex</source>
+        <translation>Hex</translation>
+    </message>
+    <message>
+        <source>Dec</source>
+        <translation>Dec</translation>
+    </message>
+    <message>
+        <source>Offset :</source>
+        <translation>Position :</translation>
+    </message>
+</context>
+<context>
+    <name>QGoToBytesController</name>
+    <message>
+        <source>No Offset has been specified</source>
+        <translation>Aucune Position n&apos;a été spécifié</translation>
+    </message>
+    <message>
+        <source>You may only give the offset as : 
+	 a hex number</source>
+        <translation>Vous ne pouvez donner la position que comme&#xa0;:
+	 un nombre hexadécimal</translation>
+    </message>
+    <message>
+        <source>You may only give the offset as : 
+	 a positive decimal number</source>
+        <translation>Vous ne pouvez donner la position que comme :
+	 un nombre décimal positif</translation>
+    </message>
+    <message>
+        <source>Radio button problem</source>
+        <translation>Problème de bouton radio</translation>
+    </message>
+</context>
+<context>
+    <name>QPreferencesFilesStructuresView</name>
+    <message>
+        <source>&amp;New one</source>
+        <translation>&amp;Nouveau</translation>
+    </message>
+    <message>
+        <source>&amp;Remove</source>
+        <translation>&amp;Retirer</translation>
+    </message>
+</context>
+<context>
+    <name>QPreferencesFilesStructuresViewController</name>
+    <message>
+        <source>XML files (*.xml)</source>
+        <translation>Fichiers XML (*.xml)</translation>
+    </message>
+    <message>
+        <source>File removed successfully</source>
+        <translation>Fichier supprimé avec succès</translation>
+    </message>
+    <message>
+        <source>Failed to remove file</source>
+        <translation>Échec de la suppression du fichier</translation>
+    </message>
+    <message>
+        <source>File does not exists</source>
+        <translation>Le fichier n&apos;existe pas</translation>
+    </message>
+    <message>
+        <source>Source file does not exist</source>
+        <translation>Le fichier source n&apos;existe pas</translation>
+    </message>
+    <message>
+        <source>No read permission for source file</source>
+        <translation>Aucune autorisation de lecture pour le fichier source</translation>
+    </message>
+    <message>
+        <source>Destination directory does not exist. Creating...</source>
+        <translation>Le répertoire de destination n&apos;existe pas. Création...</translation>
+    </message>
+    <message>
+        <source>Failed to create destination directory</source>
+        <translation>Échec de la création du répertoire de destination</translation>
+    </message>
+    <message>
+        <source>File with the same name already exists in the destination folder</source>
+        <translation>Le fichier du même nom existe déjà dans le dossier de destination</translation>
+    </message>
+    <message>
+        <source>The file already exists.</source>
+        <translation>Le fichier existe déjà.</translation>
+    </message>
+    <message>
+        <source>Do you want to change the name or remove the older one?</source>
+        <translation>Voulez-vous changer le nom ou supprimer l’ancien ?</translation>
+    </message>
+    <message>
+        <source>Change Name</source>
+        <translation>Changer de nom</translation>
+    </message>
+    <message>
+        <source>Remove Older</source>
+        <translation>Supprimer l&apos;ancien</translation>
+    </message>
+    <message>
+        <source>Change File Name</source>
+        <translation>Changer le nom du fichier</translation>
+    </message>
+    <message>
+        <source>New File Name:</source>
+        <translation>Nouveau nom de fichier&#xa0;:</translation>
+    </message>
+    <message>
+        <source>New file name was not provided</source>
+        <translation>Le nouveau nom de fichier n&apos;a pas été fourni</translation>
+    </message>
+    <message>
+        <source>Failed to remove existing destination file</source>
+        <translation>Échec de la suppression du fichier de destination existant</translation>
+    </message>
+    <message>
+        <source>Failed to copy file</source>
+        <translation>Échec de la copie du fichier</translation>
+    </message>
+    <message>
+        <source>File copied successfully</source>
+        <translation>Fichier copié avec succès</translation>
+    </message>
+</context>
+<context>
     <name>QWindowMain</name>
     <message>
-        <location filename="../src/GUI/QWindowMain.cpp" line="48"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QWindowMain.cpp" line="49"/>
         <source>&amp;Open...</source>
         <translation>&amp;Ouvrir...</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QWindowMain.cpp" line="52"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Préférences</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation>&amp;Sauvegarder</translation>
+    </message>
+    <message>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QWindowMain.cpp" line="55"/>
+        <source>&amp;View</source>
+        <translation>&amp;Voir</translation>
+    </message>
+    <message>
+        <source>&amp;Go to Bytes</source>
+        <translation>&amp;Aller aux octets</translation>
+    </message>
+    <message>
+        <source>&amp;Find</source>
+        <translation>&amp;Trouver</translation>
+    </message>
+    <message>
+        <source>&amp;Option</source>
+        <translation>&amp;Option</translation>
+    </message>
+    <message>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
-        <location filename="../src/GUI/QWindowMain.cpp" line="56"/>
         <source>&amp;About</source>
-        <translation>A &amp;propos</translation>
+        <translation>&amp;À propos</translation>
+    </message>
+</context>
+<context>
+    <name>QWindowPreferences</name>
+    <message>
+        <source>&amp;Structure Files</source>
+        <translation>&amp;Fichiers de structure</translation>
+    </message>
+    <message>
+        <source>&amp;Other</source>
+        <translation>&amp;Autre</translation>
     </message>
 </context>
 </TS>
