1d6accfb6d706b87f6968a452a17b530e3eca3e5
Raphaël Tatin <raphael.tatin@seek-one.fr>
2024-07-08 09:23:36 +0200

    Add documentation on QAboutDialog

--- src/GUI/QAboutDialog.h
+++ src/GUI/QAboutDialog.h
@@ -11,19 +11,48 @@
 #include <QDialog>
 #include <QString>
 
+/**
+ * @brief Forward declaration of QAbstractButton
+ */
 class QAbstractButton;
 
+/**
+ * @brief A dialog class to display information about the application
+ * 
+ * The QAboutDialog class provides a standard dialog that shows
+ * information about the application, including the libraries udes
+ */
 class QAboutDialog : public QDialog
 {
 	Q_OBJECT
 public:
+	/**
+	 * @brief Construct a new QAboutDialog object
+	 * 
+	 * @param parent The parent widget of the dialog. Defaults to nullprt
+	 */
 	QAboutDialog(QWidget * parent = 0);
+	/**
+	 * @brief Destroy the QAboutDialog object
+	 */
 	virtual ~QAboutDialog();
 
 public slots:
+	/**
+	 * @brief Slot to handle the 'About Qt' menu click event
+	 */
 	void menuAboutQtClicked();
 
 private:
+	/**
+	 * @brief Adds a button representing a library to the dialog
+	 * 
+	 * @param pLayout The layout to which the button will added
+	 * @param pParent The parent widget of the button
+	 * @param szIconFile The file path of the button icon
+	 * @param szName The name of the library
+	 * @return A pointer to the created QAbstractButton
+	 */
 	QAbstractButton* addLibraryButton(QLayout* pLayout, QWidget* pParent, const QString& szIconFile, const QString& szName);
 };
 
